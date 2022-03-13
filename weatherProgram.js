13b6a63daedd4131877cab0747c8f131e8affd74
Aleks Magnusson <92426232+aleksmagnusson@users.noreply.github.com>
2022-03-13 15:48:42 +0100

    Commit Done Rev.A1

---
+++ weatherProgram.js
@@ -0,0 +1,45 @@
+// Making the program from the function "calculateWeather" and it operates by the property string "validNumber".
+function calculateWeater(validNumber) {
+  // Enter any type of number in Kelvin degrees:
+  validNumber = parseFloat(validNumber);
+  // "document.getElementById" runs the Element Id you give in the HTML tags.
+  // The innerHTML property sets or returns the HTML content (inner HTML) of an element.
+  // "validNumber" is declared as parseFloat which means any type of number you put in the input field, index.html 20-26.
+  document.getElementById("outputCelcius").innerHTML = validNumber - 273.15;
+
+  // Kelvin => Celsius
+  document.getElementById("outputFahrenheit").innerHTML =
+    (validNumber - 273) * 1.8 + 32;
+
+  document.getElementById("outputNewton").innerHTML =
+    (validNumber - 273) * (33 / 100);
+}
+
+/*
+
+--> PREVIOUS CODE <--
+
+// This is the degreece in Kelvin. Declared as a constant.
+const kelvin = 293;
+
+// Celsius is 273 degreece less than Kelvin. Declared as a constant.
+const celsius = kelvin - 273;
+
+let newton = celsius * (33 / 100);
+newton = Math.floor(newton);
+
+// By calculating Fahrenheit we use the following formula below.
+// Fahrenheit = Celsius * (9/5) + 32;
+let fahrenheit = celsius * (9 / 5) + 32;
+
+// By using Math.floor() when the results ends in decimals.
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`Kelvin is '293' degrees.`);
+// Console.log the result of Fahrenheit in x degrees.
+console.log(`The temperature is ${fahrenheit} degrees in Fahrenheit.`);
+// Console.log the result of Kelvin in x degrees.
+console.log(`The temperature is ${celsius} degrees in Celsius.`);
+// Console.log the result of Newton in x degrees.
+console.log(`The temperature is ${newton} degrees in Newton.`);
+*/
